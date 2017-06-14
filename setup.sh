561e5529fe5807ce7767e5dac75f92fc57c5d706
Robert Gifford <robertgif@gmail.com>
2017-06-14 09:34:37 -0400

    Change setup.sh to source_me.sh
    - Full "Setup" -meaning automation- will be handled by master repository now
    - Script still works for running through jtag_eval with manualy typed instructions

--- setup.sh
+++
@@ -1,2 +0,0 @@
-export PATH=/opt/Xilinx/SDK/2017.1/gnu/aarch32/lin/gcc-arm-none-eabi/bin/:$PATH
-export PATH=/opt/Xilinx/SDK/2017.1/bin/:$PATH
---
+++ source_me.sh
@@ -0,0 +1,2 @@
+export PATH=~/opt/Xilinx/SDK/2017.1/gnu/aarch32/lin/gcc-arm-none-eabi/bin/:$PATH
+export PATH=~/opt/Xilinx/SDK/2017.1/bin/:$PATH
