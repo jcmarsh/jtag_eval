92f150a6f4ffc49fcdd5c3b4c58de8f8daa55048
James Marshall <jcmarsh@gwmail.gwu.edu>
2017-07-31 08:45:10 -0400

    Settigns for James' laptop.

--- source_me.sh
+++ source_me.sh
@@ -1,2 +1,1 @@
-export PATH=~/opt/Xilinx/SDK/2017.1/gnu/aarch32/lin/gcc-arm-none-eabi/bin/:$PATH
-export PATH=~/opt/Xilinx/SDK/2017.1/bin/:$PATH
+export PATH=/home/jcmarsh/research/IRAD/Xilinx/SDK/2016.3/gnu/aarch32/lin/gcc-arm-none-eabi/bin:$PATH
