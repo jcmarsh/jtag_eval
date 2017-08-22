fb853d76ad584d70daa664890253fd04f781b32e
James <jcmarsh@gwmail.gwu.edu>
2017-08-22 16:30:11 -0400

    Need a newer version of xsdb.

--- source_me_lem4.sh
+++ source_me_lem4.sh
@@ -1,1 +1,1 @@
-export PATH=/home/jcmarsh/research/IRAD/Xilinx/SDK/2016.2/gnu/aarch32/lin/gcc-arm-none-eabi/bin:$PATH
+export PATH=/home/jcmarsh/research/Xilinx/SDK/2017.2/gnu/aarch32/lin/gcc-arm-none-eabi/bin:$PATH
