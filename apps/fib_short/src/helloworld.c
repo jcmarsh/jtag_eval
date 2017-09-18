30a0fe050b804d343e4c7e017c4e0dd3d5239a4f
James <jcmarsh@gwmail.gwu.edu>
2017-09-18 10:17:32 -0400

    Add short fib program for jcmarsh/master_jtag#9

---
+++ apps/fib_short/src/helloworld.c
@@ -0,0 +1,69 @@
+/*****************************************************
+Getting Started Guide for Zybo
+
+This demo displays the status of the switches on the
+LEDs and prints a message to the serial communication
+when a button is pressed.
+
+Terminal Settings:
+   -Baud: 115200
+   -Data bits: 8
+   -Parity: no
+   -Stop bits: 1
+
+1/6/14: Created by MarshallW
+
+Modifying to make a simple program to inject faults into.
+Meant to run faster than fib_rec - JM
+****************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "platform.h"
+#include <xgpio.h>
+#include <unistd.h>
+
+int fib_i(int n) {
+	int ii;
+	int prev_0 = 1;
+	int prev_1 = 1;
+	int old_val = 0;
+
+	for (ii = 2; ii < n; ii++) {
+		old_val = prev_0;
+		prev_0 += prev_1;
+		prev_1 = old_val;
+	}
+	return prev_0;
+}
+
+#define FIB_RESULT 144 // Assuming you start with 1, 1, 2, 3, 5...
+#define FIB_COUNT 12 // 40 takes about 15 seconds
+
+int main() {
+  int fib_out = 0;
+
+  init_platform();
+
+  /* Set a breakpoint on this label to let DrSEUS restart exectuion when readdy. */
+  asm("drseus_start_tag:");
+
+  print("Starting program\n\r");
+  
+  fib_out = fib_i(FIB_COUNT);
+  
+  if (fib_out != FIB_RESULT) {
+    xil_printf("Fibs do not match: %d, %d %d\n\r", FIB_COUNT, fib_out, FIB_RESULT);
+    xil_printf("FAULT (%d, %d); %d\n\r", FIB_COUNT, FIB_RESULT, fib_out);
+  } else {
+    xil_printf("Result: %d, %d\n\r", FIB_COUNT, fib_out);
+  }
+  if (fib_out < 0) {
+    xil_printf("Overflow: %d, %d %d\n\r", FIB_COUNT, fib_out);
+  }
+
+  asm("drseus_end_tag:");
+  print("safeword ");
+  cleanup_platform();
+  return 0;
+}
