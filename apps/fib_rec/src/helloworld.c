3b926b528781030797ec7f7ac07d13c0072694ae
James Marshall <jcmarsh@gwmail.gwu.edu>
2018-02-22 15:39:21 -0500

    Cleanup platform.c and fib_rec.

--- apps/fib_rec/src/helloworld.c
+++ apps/fib_rec/src/helloworld.c
@@ -54,62 +54,38 @@ int fib_r(int n) {
 
 int main()
 {
-   //XGpio input, output;
-	XGpio output;
 	int fib_out_i = 0;
 	int fib_out_r = 0;
-	int loop_count = 0;
-	// int switch_data = 0;
-	// int switch_data_error = 0;
 
-	// XGpio_Initialize(&input, XPAR_AXI_GPIO_0_DEVICE_ID);	//initialize input XGpio variable
-	XGpio_Initialize(&output, XPAR_AXI_GPIO_1_DEVICE_ID);	//initialize output XGpio variable
-
-	// XGpio_SetDataDirection(&input, 1, 0xF);			//set first channel tristate buffer to input
-	// XGpio_SetDataDirection(&input, 2, 0xF);			//set second channel tristate buffer to input
-
-	XGpio_SetDataDirection(&output, 1, 0x0);		//set first channel tristate buffer to output
 	init_platform();
 
 	print("Starting program\n\r");
 
-	// Run a few times to warm caches before test section
-	fib_out_i = fib_i(FIB_COUNT);
-	fib_out_r = fib_r(FIB_COUNT);
-	xil_printf("Result: %d, %d\n\r", fib_out_i, fib_out_r);
-	fib_out_i = fib_i(FIB_COUNT);
-	fib_out_r = fib_r(FIB_COUNT);
-	xil_printf("Result: %d, %d\n\r", fib_out_i, fib_out_r);
+	// Run once to warm caches before test section
 	fib_out_i = fib_i(FIB_COUNT);
 	fib_out_r = fib_r(FIB_COUNT);
 	xil_printf("Result: %d, %d\n\r", fib_out_i, fib_out_r);
 
-        /* Set a breakpoint on this label to let DrSEUS restart exectuion when readdy. */
+        /* Set a breakpoint on this label to let DrSEUS restart exectuion when ready. */
         asm("drseus_start_tag:");
 
-	while(loop_count < 3) {
-	//while(1) {
-		xil_printf("Starting big loop: %d\n\r", loop_count++);
+        fib_out_i = fib_i(FIB_COUNT);
+        fib_out_r = fib_r(FIB_COUNT);
+	xil_printf("Result: %d, %d\n\r", fib_out_i, fib_out_r);
 
-		// xil_printf("sizeof int: %d, sizeof long: %d\n", sizeof(int), sizeof(long));
-		// switch_data = (switch_data + 1) % 2;	// flip light
-		// XGpio_DiscreteWrite(&output, 1, switch_data | switch_data_error);	//write switch data to the LEDs
-
-		fib_out_i = fib_i(FIB_COUNT);
-		fib_out_r = fib_r(FIB_COUNT);
-
-                if (fib_out_i != fib_out_r) {
-			xil_printf("Fibs do not match: %d, %d %d\n\r", FIB_COUNT, fib_out_i, fib_out_r);
-                } else {
-			xil_printf("Result: %d, %d\n\r", fib_out_i, fib_out_r);
-		}
-		if (fib_out_i < 0 || fib_out_r < 0) {
-			xil_printf("Fib overflow: %d, %d %d\n\r", FIB_COUNT, fib_out_i, fib_out_r);
-		}
-	}
+        /* Not currently interested in the program detecitng errors. 
+        if (fib_out_i != fib_out_r) {
+          xil_printf("Fibs do not match: %d, %d %d\n\r", FIB_COUNT, fib_out_i, fib_out_r);
+        } else {
+          xil_printf("Result: %d, %d\n\r", fib_out_i, fib_out_r);
+        }
+        if (fib_out_i < 0 || fib_out_r < 0) {
+          xil_printf("Fib overflow: %d, %d %d\n\r", FIB_COUNT, fib_out_i, fib_out_r);
+        }
+        */
 
 	asm("drseus_end_tag:");
-	xil_printf("safeword ");
-	cleanup_platform();
-	return 0;
+        xil_printf("safeword ");
+
+        return 0;
 }
