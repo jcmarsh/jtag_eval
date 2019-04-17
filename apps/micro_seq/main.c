39c6cbff5086148aadf43e5895993a70df05311b
James Marshall <jcmarsh@gwmail.gwu.edu>
2019-04-16 20:24:12 -0400

    Trying more platform / benchmark combinations.

--- apps/micro_seq/main.c
+++ apps/micro_seq/main.c
@@ -18,6 +18,11 @@ int main(int argc, char *argv[]) {
   int sum_0 = 0, count_0 = 0;
   int index;
 
+  // Test with heap instead of stack data
+  //char * buffer;
+  //buffer = malloc(max_data_size);
+  //memcpy(buffer, kepler_data, max_data_size);
+
   Xil_L2CacheFlush();
   asm("drseus_start_tag:");
 
@@ -25,6 +30,7 @@ int main(int argc, char *argv[]) {
   for (index = 0; index < max_data_size; index++) {
     // printf("Word[%d]: %d\n", index, kepler_data[index]);
     sum_0 += kepler_data[index];
+    //sum_0 += buffer[index];
     count_0++;
   }
   asm("drseus_end_tag:");
