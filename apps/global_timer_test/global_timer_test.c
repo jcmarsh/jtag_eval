1e7a33d580248cba31b22ed2aaded41893b3b06e
James Marshall <jcmarsh@gwmail.gwu.edu>
2019-04-17 09:19:24 -0400

    Global timer seems to work fine.

--- apps/global_timer_test/global_timer_test.c
+++ apps/global_timer_test/global_timer_test.c
@@ -27,15 +27,38 @@ crash the system... guessing it overwrote the stack.
 #include "sleep.h"
 #include <unistd.h>
 
+// CPU Clock / 2
+#define TIMER_MHZ 333
+
 // For Cache Flush
 #include <xil_cache_l.h>
 
 int main()
 {
+	uint64_t old_time, new_time, delta;
+
 	init_platform();
 
 	print("Starting program\n\r");
 
 	// Just set time, get current time, sleep 1s, get new time, print
+	XTime_GetTime(&old_time);
+	printf("Current time %llu\n", old_time);
+
+	XTime_SetTime(0);
+	XTime_GetTime(&old_time);
+	printf("Reset time %llu\n", old_time);
+
+	for (int i = 0; i < 10; i++) {
+		XTime_GetTime(&old_time);
+		sleep(10);
+		//usleep(1500);
+		XTime_GetTime(&new_time);
+
+		delta = new_time - old_time;
+		printf("Sleep(10) timed: %llu - %llu = %llu\n", new_time, old_time, delta);
+		printf("Delta in ms: %llu\n", delta / TIMER_MHZ);
+	}
+
 	return 0;
 }
